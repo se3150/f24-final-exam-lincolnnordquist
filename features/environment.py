eeaaddc85cf851bb88df32020b2931814e343c71
Lincoln Nordquist <lincolnnordquist@gmail.com>
2024-12-09 14:08:38 -0700

    plz work actions

--- features/environment.py
+++ features/environment.py
@@ -1,20 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import behave_webdriver
 
 def before_all(context):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-
-    context.behave_driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
-    context.behave_driver.implicitly_wait(10)
+    context.behave_driver = behave_webdriver.Chrome.headless()
 
 def after_all(context):
-    if hasattr(context, 'behave_driver'):
-        context.behave_driver.quit()
+    context.behave_driver.quit()
