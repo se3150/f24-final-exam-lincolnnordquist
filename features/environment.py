4bcb30c8c94b152d9e216a47b0d8c4300fd1ac43
Lincoln Nordquist <lincolnnordquist@gmail.com>
2024-12-09 14:11:19 -0700

    trying again

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,8 @@
 import behave_webdriver
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome.headless()
+    context.behave_driver = behave_webdriver.Chrome(headless=True)
 
 def after_all(context):
-    context.behave_driver.quit()
+    if hasattr(context, 'behave_driver'):
+        context.behave_driver.quit()
